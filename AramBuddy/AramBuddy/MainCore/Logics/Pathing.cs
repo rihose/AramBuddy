d9c4e36140dc6f597c9dc6ed086173ad31352a91
rihose <arisinterlude@hotmail.com>
2017-03-03 18:31:41 +0200

    Update Pathing.cs

--- AramBuddy/AramBuddy/MainCore/Logics/Pathing.cs
+++ AramBuddy/AramBuddy/MainCore/Logics/Pathing.cs
@@ -152,28 +152,6 @@ namespace AramBuddy.MainCore.Logics
                 return;
             }*/
 
-            // Stays Under tower if the bot health under 10%.
-            if ((ModesManager.CurrentMode == ModesManager.Modes.Flee || (Player.Instance.PredictHealthPercent() < 10 && Player.Instance.CountAllyHeros(SafeValue + 2000) < 3))
-                && EntityManager.Heroes.Enemies.Count(e => e.IsValid && !e.IsDead && e.IsInRange(Player.Instance, SafeValue + 200)) > 0)
-              
-                {
-                if (ObjectsManager.SafeAllyTurret != null)
-                {
-                    Program.Moveto = "NearestAlly";
-                    Position = ObjectsManager.SafeAllyTurret.PredictPosition().Random().Extend(ObjectsManager.AllySpawn.Position.Random(), 400).To3D();
-                    return;
-                }
-            }
-                
-            if (Player.Instance.GetAutoAttackRange() < 425)
-            {
-                MeleeLogic();
-            }
-            else
-            {
-                RangedLogic();
-            }
-        }
 
         /// <summary>
         ///     Melee Champions Logic.
